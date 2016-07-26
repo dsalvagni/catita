21665675c55264c1111bbee20ff1efe3893d77f2
Daniel Salvagni <danielsalvagni@gmail.com>
2016-07-26 14:28:27 -0300

    Update create-database-and-permissions-development.sql

--- scripts/create-database-and-permissions-development.sql
+++ scripts/create-database-and-permissions-development.sql
@@ -1,3 +1,3 @@
-CREATE DATABASE catita_development;
+CREATE DATABASE IF NOT EXISTS  catita_development;
 CREATE USER 'catita_dev'@'localhost' IDENTIFIED BY 'secret';
 GRANT ALL ON catita_development.* TO 'catita_test'@'localhost';
