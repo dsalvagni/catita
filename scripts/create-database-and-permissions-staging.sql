ece4d41291dccd907a8a197796995d927f78d2bb
Daniel Salvagni <danielsalvagni@gmail.com>
2016-07-26 14:23:06 -0300

    Update create-database-and-permissions-staging.sql
    Fix `CREATE DATABASE` statement

--- scripts/create-database-and-permissions-staging.sql
+++ scripts/create-database-and-permissions-staging.sql
@@ -1,3 +1,3 @@
-CREATE DATABASE 'catita_staging';
+CREATE DATABASE catita_staging;
 CREATE USER 'catita_stag'@'localhost' IDENTIFIED BY 'secret';
 GRANT ALL ON catita_staging.* TO 'catita_stag'@'localhost';
